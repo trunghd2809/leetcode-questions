ecc486fb0f1c23d40dd8ccca8ac1f84dcdea665b
Hoang Trung <hoangtrung@Hoangs-MacBook-Pro.local>
2023-07-12 15:10:36 +0700

    Arrays

--- arrays-find-minimum-maximum-element.js
+++ arrays-find-minimum-maximum-element.js
@@ -22,3 +22,18 @@ const findMinMax = (nums) => {
 }
 
 console.log(findMinMax([1, 423, 6, 46, 34, 23, 13, 53, 4]))
+
+/**
+ * Solve problem by recursion
+ * stop n = 0
+ * n = array.length
+ * i = n - 1
+ * !! Time complexity O(n)
+ * !! Space complexity O(n) due to recursion
+ */
+
+const findMin = (nums, i, n) => {
+  return n === 0 ? nums[n] : Math.min(nums[i], findMin(nums, i - 1, n - 1));
+}
+
+console.log(findMin([4, 423, 6, 46, 2, 23, 13, 53, 4], 8, 7))
