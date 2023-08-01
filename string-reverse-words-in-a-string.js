984b82dbd8962de524f450d3bf194aed9ac30d79
Hoang Trung <hoangtrung@Hoangs-MacBook-Pro.local>
2023-08-01 21:44:11 +0700

    String reverse words in a string

---
+++ string-reverse-words-in-a-string.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseWords = function(s) {
+  const arr = s.trim().replaceAll(/\s{1,}/gm, ' ').split(' ');
+  let left = 0;
+  let right = arr.length - 1;
+  
+  while (left < right) {
+    let tem =  arr[right];
+    arr[right] = arr[left];
+    arr[left] = tem;
+    right -= 1; left += 1;
+  }
+  
+  return arr.join(' ');
+};
+console.log(reverseWords("  Bob    Loves  Alice   "));
