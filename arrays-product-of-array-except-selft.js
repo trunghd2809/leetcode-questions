670f6722e88cc4b738a97db06aa2376382d793ea
Hoang Trung <hoangtrung@Hoangs-MacBook-Pro.local>
2023-08-02 00:30:08 +0700

    Arrays product of array except self

---
+++ arrays-product-of-array-except-selft.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * Using prefix sum
+ */
+const productExceptSelf = function(nums) {
+  const arr = [nums[0]];
+
+  for (let i = 1; i< nums.length; i++) {
+    arr[i] = arr[i - 1] * nums[i];
+  }
+  let productVal = 1;
+  for (let i = nums.length - 1; i >= 0 ; i--) {
+    arr[i] = i === 0 ? productVal : arr[i - 1] * productVal;
+    productVal *= nums[i];
+  }
+
+  return arr;
+};
+console.log(productExceptSelf([1,2,3,4]))
