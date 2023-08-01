08a88b2ff4bbcc46830ce8031c91dde337f8e470
Hoang Trung <hoangtrung@Hoangs-MacBook-Pro.local>
2023-08-01 15:41:53 +0700

    Array/String Greatest common divisor of strings

---
+++ string-greatest-common-divisor-of-strings.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ * O(n*m^2)
+ */
+const gcdOfStrings = (str1, str2) => {
+  if (str1 + str2 != str2 + str1) return '';
+  const isValid = (sub, str) => {
+    if (str.length === 0) return true;
+    if (!str.startsWith(sub)) return false;
+    return isValid(sub, str.slice(sub.length));
+  }
+
+  let res = '';
+  for (let i = 1; i <= str1.length; i++) { // 0(n)
+    const sub = str1.slice(0, i);
+    if (isValid(sub, str1) && isValid(sub, str2)) res = sub; 
+  }
+  return res;
+};
+console.log(gcdOfStrings('ABCABC', 'ABC'));
