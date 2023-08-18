1058fcb634de7240f9c4a852533084a76413dfb4
Hoang Trung <hoangtrung@Hoangs-MacBook-Pro.local>
2023-08-18 17:10:12 +0700

    Sliding window max consecutive ones iii

---
+++ sliding-window-max-consecutive-ones-iii.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+// TODO improve in the feature
+const longestOnes = function(nums, k) {
+  let count = 0;
+  const flip = k;
+  let pointer = 0;
+
+  // step 1: Initialize pointers and calculate initial max
+  for (let i = 0; i < flip; i++) {
+    if (nums[i] === 0) {
+      k--; count++;
+    } else count++;
+  }
+
+  let max = count;
+  // step 2: calculate max
+  for (let right = flip; right < nums.length; right++) {
+    if (nums[right] === 1) {
+      count++;
+    } else {
+      if (k === 0) {
+        right = pointer; // because right++ in end of condition for loop
+        pointer++;
+        k = flip; count = 0;
+      } else {
+        k--; count++;
+      }
+    }
+    max = Math.max(max, count);
+  }
+  return max;
+};
+console.log(longestOnes([0,1,1], 0));
