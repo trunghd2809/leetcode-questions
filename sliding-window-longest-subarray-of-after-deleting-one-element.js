7e060497d6b3131941b330b47bdcc4f44563c488
Hoang Duc Trung <hoangtrungmta94@gmail.com>
2023-08-18 22:56:32 +0700

    Sliding window longest subarray of after deleting one element

---
+++ sliding-window-longest-subarray-of-after-deleting-one-element.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// draft and check leetcode done all case
+const longestSubarrayPrev = function(nums) {
+  let start = 0, end = 0;
+  let deleteEl = 0;
+  let max = 0, count = 0;
+  let isNotZero = true;
+
+  while(end < nums.length) {
+    if (!nums[end]) {
+      deleteEl++; isNotZero = false;
+      if (deleteEl > 1) {
+        while(true) {
+          if (!nums[start]) {
+            start++; deleteEl--;
+            break;
+          } else {
+            start++; count--;
+          }
+        }
+      }
+    } else {
+      count++;
+    }
+    end++;
+    max = Math.max(max, count);
+  }
+  return isNotZero ? max - 1 : max;
+};
+// optimize unnecessary variables
+
+const longestSubarray = (nums) => {
+  let start = 0, maxCount = 0, zeroCount = 0;
+
+  for (let end = 0; end < nums.length; end++) {
+    if (nums[end] === 0) zeroCount++;
+
+    while(zeroCount > 1) {
+      if (nums[start] === 0) {
+        zeroCount--;
+      }
+      start++;
+    }
+
+    maxCount = Math.max(maxCount, end - start);
+  }
+
+  return maxCount;
+}
+console.log(longestSubarray([1,1,0,0,1,1,1,0,1]));
