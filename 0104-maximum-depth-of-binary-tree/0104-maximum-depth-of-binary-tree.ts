1bfb8755ba749fb5ca2ad4089a15756e34715745
Hoàng Đức Trung <38755997+trunghd2809@users.noreply.github.com>
2023-08-28 17:23:05 +0700

    Time: 65 ms (65.71%), Space: 46.1 MB (78.99%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+ if (root === null) return 0;
+  
+  let maxLef = maxDepth(root.left) + 1;
+  let maxRight = maxDepth(root.right) + 1;
+  return Math.max(maxLef, maxRight);
+};
