102173124d4426b8361e8004e8ad1cc5541b9f47
Hoàng Đức Trung <38755997+trunghd2809@users.noreply.github.com>
2023-08-28 16:41:17 +0700

    Time: 68 ms (68.71%), Space: 49.9 MB (23.31%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const maxArea = function(height) {
+  let i = 0;
+  let j = height.length - 1;
+  let max = 0;
+
+  while (i < j) {
+    const left = height[i];
+    const right = height[j];
+    const width = j - i;
+    const maxStore = Math.min(left, right) * width;
+    max = Math.max(max, maxStore);
+    Math.min(left, right) == left ? i++ : j--;
+  }
+
+  return max;
+};
