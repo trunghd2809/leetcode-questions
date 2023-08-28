600bb99c438c841c44f170af0f0025c42adf5b3a
Hoàng Đức Trung <38755997+trunghd2809@users.noreply.github.com>
2023-08-28 16:45:26 +0700

    Time: 64 ms (89.28%), Space: 48.4 MB (64.89%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+const reverseVowels = function(s) {
+  let arr = s.split('');
+  const vowels = ['a','e','i','o','u','A','E','I','O','U'];
+  let leftPointer = 0;
+  let rightPointer =  s.length - 1;
+
+  while (leftPointer <= rightPointer) {
+    if (vowels.includes(arr[leftPointer]) && vowels.includes(arr[rightPointer])) {
+      let tem = arr[rightPointer];
+      arr[rightPointer] = arr[leftPointer];
+      arr[leftPointer] = tem;
+      leftPointer++;
+      rightPointer--;
+    } else if (!vowels.includes(arr[leftPointer]) && !vowels.includes(arr[rightPointer])) {
+      leftPointer++;
+      rightPointer--;
+    } else if (!vowels.includes(arr[leftPointer])) {
+      leftPointer++;
+    } else {
+      rightPointer--;
+    }
+  }
+  return arr.join('');
+};
