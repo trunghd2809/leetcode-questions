f2ba54d1776e2b478b15a388a9c0ffd89753f0ed
Hoàng Đức Trung <38755997+trunghd2809@users.noreply.github.com>
2023-08-28 16:32:24 +0700

    Time: 58 ms (82.12%), Space: 42.9 MB (38.90%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = (nums, target) => {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+  return {};
+}
