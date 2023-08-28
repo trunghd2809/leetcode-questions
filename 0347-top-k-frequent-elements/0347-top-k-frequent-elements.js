9aa5b520ec55a3f0a602c06a6fbc3a86cc159d7f
Hoàng Đức Trung <38755997+trunghd2809@users.noreply.github.com>
2023-08-28 17:32:59 +0700

    Time: 101 ms (10.42%), Space: 49.4 MB (11.29%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,31 @@
+const topKFrequent = (nums, k) => {
+  // create map store value and concurrences
+  const map = new Map();
+
+  for(let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      const v = map.get(nums[i]);
+      map.set(nums[i], v + 1);
+    } else {
+      map.set(nums[i], 0);
+    }
+  }
+
+  // Use heap to store k elements
+  const heap = [];
+  [...map.entries()].forEach(([key, value], index) => {
+    if (index < k) {
+      heap[index] = { item: key, priority: value };
+    } else {
+      heap.sort((a, b) => b.priority - a.priority);
+      if (value > heap[k-1].priority) {
+        heap[k-1] = { item: key, priority: value };
+        heap.sort((a, b) => b.priority - a.priority);
+      }
+    }
+  });
+
+  console.log(heap);
+  // return array of most k frequent elements
+  return heap.slice(0, k).map((i) => i.item);;
+};
