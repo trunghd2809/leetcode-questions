2e4cc9953ab8786826e6cb54ffed535ed010995f
Hoàng Đức Trung <38755997+trunghd2809@users.noreply.github.com>
2023-10-18 10:50:22 +0700

    Time: 1855 ms (75.54%), Space: 0B (100%) - LeetHub

--- 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/1174-sales-analysis-iii.sql
@@ -3,10 +3,10 @@ select
   p.product_id,
   p.product_name
 from product p
-inner join (
+where p.product_id in (
   select
     product_id
   from sales
   group by product_id
   having max(sale_date) <= '2019-03-31' and min(sale_date) >= '2019-01-01'
-) s on s.product_id = p.product_id
+)
